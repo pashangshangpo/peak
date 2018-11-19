1e30915331cd9327dcead95441443f075b9354ab
爬上上坡 <pashangshangpo@gmail.com>
2018-11-19 19:27:03 +0800

    添加获取随机端口

--- bin/server.js
+++ bin/server.js
@@ -3,7 +3,7 @@ const Express = require('express')
 const WebpackDevMiddleware = require('webpack-dev-middleware')
 const WebpackHotMiddleware = require('webpack-hot-middleware')
 
-const { ResolveBin } = require('./util')
+const { ResolveBin, GetRandomPort } = require('./util')
 
 module.exports = config => {
   const App = new Express()
@@ -25,29 +25,34 @@ module.exports = config => {
     log: false
   })
 
-  App
-  .use((req, res, next) => {
-    let url = req.url.slice(1)
+  GetRandomPort(port => {
+    App
+    .use((req, res, next) => {
+      let url = req.url.slice(1)
 
-    if (url === '') {
-      url = 'index.html'
-    }
+      if (url === '') {
+        url = 'index.html'
+      }
 
-    if (url === config.templateName) {
-      let template = config.template
-      template = template.replace('<!-- inject script -->', '<script src="index.js"></script>')
+      if (url === config.templateName) {
+        let template = config.template
+        template = template.replace('<!-- inject script -->', '<script src="index.js"></script>')
 
-      res.end(template)
-    }
-    else {
-      next()
-    }
-  })
-  .use(DevMiddleware)
-  .use(HotMiddleware)
-  .listen(8087, err => {
-    if (err) {
-      throw new Error(err)
-    }
+        res.end(template)
+      }
+      else {
+        next()
+      }
+    })
+    .use(DevMiddleware)
+    .use(HotMiddleware)
+    .listen(port, err => {
+      if (err) {
+        throw new Error(err)
+      }
+      else {
+        console.log(`http://localhost:${port}`)
+      }
+    })
   })
 }
--- bin/util.js
+++ bin/util.js
@@ -1,4 +1,23 @@
 const Path = require('path')
+const Net = require('net')
+
+const GetRandomPort = (cb, port = 8089) => {
+  const Server = Net.createServer().listen(port)
+  
+  Server.on('listening', () => {
+    Server.once('close', () => {
+      cb(port)
+    })
+
+    Server.close()
+  })
+
+  Server.on('error', err => {
+    if (err.code === 'EADDRINUSE') {
+      GetRandomPort(cb, port + 1)
+    }
+  })
+}
 
 module.exports = {
   ResolveRoot(...arg) {
@@ -6,5 +25,6 @@ module.exports = {
   },
   ResolveBin(...arg) {
     return Path.join(__dirname, ...arg)
-  }
+  },
+  GetRandomPort: GetRandomPort
 }
