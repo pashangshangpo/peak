efcfe0bd3083561f517e38e9679dbb9083d1b0b6
爬上上坡 <pashangshangpo@gmail.com>
2018-11-22 17:08:54 +0800

    输出 html 文件并替换相应资源

--- bin/build.js
+++ bin/build.js
@@ -1,4 +1,53 @@
+const Fs = require('fs')
+const Path = require('path')
 const Webpack = require('webpack')
+
+const GetAssets = stats => {
+  let res = {}
+  let assets = stats.toJson({
+    all: false,
+    assets: true
+  }).assets
+
+  for (let item of assets) {
+    let ext = Path.extname(item.name).slice(1)
+    
+    if (res[ext] == null) {
+      res[ext] = []
+    }
+
+    res[ext].push(item.name)
+  }
+
+  return res
+}
+
+const ReplaceTemplate = (assets, template, injectScript) => {
+  let temp = template
+
+  if (assets.css) {
+    temp = temp.replace(
+      '<!-- inject style -->',
+      `
+        ${assets.css.map(path => {
+          return `<link href="/${path}" rel="stylesheet">`
+        }).join('')}
+      `
+    )
+  }
+
+  temp = temp.replace(
+    '<!-- inject script -->',
+    `
+      ${injectScript}
+      ${assets.js.map(path => {
+        return `<script src="/${path}"></script>`
+      }).join('')}
+    `
+  )
+
+  return temp
+}
 
 module.exports = config => {
   console.log('请稍等，正在编译中...')
@@ -8,6 +57,17 @@ module.exports = config => {
       throw new Error(err)
     }
     else {
+      let template = ReplaceTemplate(
+        GetAssets(stats),
+        config.template,
+        config.injectScript
+      )
+
+      Fs.writeFileSync(
+        Path.join(config.webpackConfigProd.output.path, config.templateName),
+        template
+      )
+      
       console.log(stats.toString({
         all: false,
         assets: true,
