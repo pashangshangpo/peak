c784820a1718b76ca623fb45cf34aac3ceb5c357
pashangshangpo <pashangshangpo@gmail.com>
2018-12-21 14:29:25 +0800

    chore: 不限制同源策略

--- bin/types/server.js
+++ bin/types/server.js
@@ -32,8 +32,8 @@ module.exports = (config, port) => {
 
   App.all('*', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept, X-Requested-With')
-    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', '*')
+    res.header('Access-Control-Allow-Methods', '*')
     
     if(req.method === 'OPTIONS') {
       res.sendStatus(200)
