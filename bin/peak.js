ec3cf246906cc42727f3b35c1acbd868ba05ff55
pashangshangpo <pashangshangpo@gmail.com>
2019-04-01 18:07:53 +0800

    feact: 添加支持设置固定端口

--- bin/peak.js
+++ bin/peak.js
@@ -8,20 +8,21 @@ Promise.resolve().then(async () => {
   const { Shell } = require('@xiaozhihua/shell-tool')
 
   const { ResolveRoot, GetRandomPort, GetIp } = require('./lib/util')
-  const Port = await new Promise(resolve => {
-    GetRandomPort(port => {
-      resolve(port)
-    })
-  })
-  
-  const Ip = GetIp()
 
   Cli
     .version('0.0.1')
     .description('前端工程化解决方案')
     .option('-t, --type [type]', '执行的命令类型，示例：server, build')
     .option('-e, --env [env]', '当前环境，会被注入到 process.env 中')
+    .option('-p, --port [port]', '固定端口，默认随机')
     .parse(process.argv)
+
+  const Ip = GetIp()
+  const Port = Cli.port || await new Promise(resolve => {
+    GetRandomPort(port => {
+      resolve(port)
+    })
+  })
     
   const ParseConfig = config => {
     let templatePath = ResolveRoot(config.template)
